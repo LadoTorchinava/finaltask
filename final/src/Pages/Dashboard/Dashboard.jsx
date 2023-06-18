495ad2065101e2fa9da7509bddf0477d73c23b0a
ladotorchinava <ladotorchinava17@gmail.com>
2023-06-18 18:20:13 +0400

    again cards

--- final/src/Pages/Dashboard/Dashboard.jsx
+++ final/src/Pages/Dashboard/Dashboard.jsx
@@ -1,17 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
 
 const API_KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
 const BASE_URL = "https://api.themoviedb.org/3";
-const ROUTE_URL = "/discover/movie";
-const QUERY_PARAMS = "?sort_by=popularity.desc&page=1&api_key=" + API_KEY;
+const ROUTE_URL = "/search/movie";
+const QUERY_PARAMS = "?query=terminator&page=1&api_key=" + API_KEY;
 const fullURL = BASE_URL + ROUTE_URL + QUERY_PARAMS;
 
+// https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1
+
 const Dashboard = () => {
+  const [movieList, setMovieList] = useState([]);
   async function getPosts() {
     try {
       const response = await fetch(fullURL);
       const data = await response.json();
       console.log(data);
+      setMovieList(data.results);
     } catch (error) {
       console.error(error);
     }
@@ -22,7 +27,10 @@ const Dashboard = () => {
 
   return (
     <>
-      <h1>something</h1>
+      <h1>movies</h1>
+      {movieList.map((movie) => {
+        return <Card movie={movie} />;
+      })}
     </>
   );
 };
