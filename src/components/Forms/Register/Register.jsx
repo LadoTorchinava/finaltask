9681f43df357512e80a9eb24a652019d35b06a5b
ladotorchinava <ladotorchinava17@gmail.com>
2023-06-21 14:19:25 +0400

    login css

--- src/components/Forms/Register/Register.jsx
+++ src/components/Forms/Register/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import "./Register.css";
 import validateRegister from "./validate.js";
 
@@ -38,82 +38,83 @@ const Register = ({ setIsRegister }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        UserName:
-        <input
-          type="text"
-          name="userName"
-          value={registerData.userName}
-          onChange={handleChange}
-        />
-        {loginErrors.userName && <span>{loginErrors.userName}</span>}
-      </label>
-      <label>
-        First Name:
-        <input
-          type="text"
-          name="firstName"
-          value={registerData.firstName}
-          onChange={handleChange}
-        />
-        {loginErrors.firstName && <span>{loginErrors.firstName}</span>}
-      </label>
-      <label>
-        Last Name:
-        <input
-          type="text"
-          name="lastName"
-          value={registerData.lastName}
-          onChange={handleChange}
-        />
-        {loginErrors.lastName && <span>{loginErrors.lastName}</span>}
-      </label>
-      <label>
-        Phone Number:
-        <input
-          type="text"
-          name="phoneNumber"
-          value={registerData.phoneNumber}
-          onChange={handleChange}
-        />
-        {loginErrors.phoneNumber && <span>{loginErrors.phoneNumber}</span>}
-      </label>
-      <label>
-        Email:
-        <input
-          type="text"
-          name="email"
-          value={registerData.email}
-          onChange={handleChange}
-        />
-        {loginErrors.email && <span>{loginErrors.email}</span>}
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={registerData.password}
-          onChange={handleChange}
-        />
-        {loginErrors.password && <span>{loginErrors.password}</span>}
-      </label>
-      <label>
-        Repeat Password:
-        <input
-          type="password"
-          name="passwordRepeat"
-          value={registerData.passwordRepeat}
-          onChange={handleChange}
-        />
-        {loginErrors.passwordRepeat && (
-          <span>{loginErrors.passwordRepeat}</span>
-        )}
-      </label>
-      <br />
-      <Button type="submit">Register</Button>
-      <Button onClick={() => setIsRegister(false)}>Sign In</Button>
+    <form onSubmit={handleSubmit} className="form">
+      <TextField
+        className="input"
+        type="text"
+        label="UserName:"
+        name="userName"
+        value={registerData.userName}
+        onChange={handleChange}
+        error={loginErrors.userName}
+        variant="outlined"
+      />
+      <TextField
+        className="input"
+        type="text"
+        label="First Name:"
+        name="firstName"
+        value={registerData.firstName}
+        onChange={handleChange}
+        error={loginErrors.firstName}
+        variant="outlined"
+      />
+      <TextField
+        className="input"
+        type="text"
+        label="Last Name:"
+        name="lastName"
+        value={registerData.lastName}
+        onChange={handleChange}
+        error={loginErrors.lastName}
+        variant="outlined"
+      />
+      <TextField
+        className="input"
+        type="text"
+        label="Phone Number:"
+        name="phoneNumber"
+        value={registerData.phoneNumber}
+        onChange={handleChange}
+        error={loginErrors.phoneNumber}
+        variant="outlined"
+      />
+      <TextField
+        className="input"
+        type="text"
+        label="Email@:"
+        name="email"
+        value={registerData.email}
+        onChange={handleChange}
+        error={loginErrors.email}
+        variant="outlined"
+      />
+      <TextField
+        className="input"
+        type="password"
+        label="Password:"
+        name="password"
+        value={registerData.password}
+        onChange={handleChange}
+        error={loginErrors.password}
+        variant="outlined"
+      />
+      <TextField
+        className="input"
+        type="password"
+        label="Repeat Password:"
+        name="passwordRepeat"
+        value={registerData.passwordRepeat}
+        onChange={handleChange}
+        error={loginErrors.passwordRepeat}
+        variant="outlined"
+      />
+      <div className="bottom">
+        <Button type="submit" variant="contained">
+          Register
+        </Button>
+        <Button onClick={() => setIsRegister(false)}>Sign In</Button>
+      </div>
     </form>
   );
 };
