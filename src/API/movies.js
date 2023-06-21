1f2fe6d905b89920ca014ccb1b29784c8cad8080
ladotorchinava <ladotorchinava17@gmail.com>
2023-06-22 00:55:02 +0400

    movie details api

--- src/API/movies.js
+++ src/API/movies.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
 const BASE_URL = "https://api.themoviedb.org/3";
-const BASE_QUERY = `?api_key=${API_KEY}&page=1`;
+const BASE_QUERY = `?api_key=${API_KEY}`;
 
 const client = axios.create({
   baseURL: BASE_URL,
@@ -10,7 +10,7 @@ const client = axios.create({
 
 export async function getRandomMovies() {
   try {
-    const { data } = await client.get(`/discover/movie${BASE_QUERY}`);
+    const { data } = await client.get(`/discover/movie${BASE_QUERY}&page=1`);
 
     return data.results;
   } catch (error) {
@@ -22,10 +22,21 @@ export async function getRandomMovies() {
 export async function getSearchMovies(query) {
   try {
     const { data } = await client.get(
-      `/search/movie${BASE_QUERY}&query=${query}`
+      `/search/movie${BASE_QUERY}&query=${query}&page=1`
     );
 
     return data.results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getMovieDetails(id) {
+  try {
+    const { data } = await client.get(`/movie/${id}${BASE_QUERY}`);
+
+    return data;
   } catch (error) {
     console.error(error);
     return [];
--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import { getMovieDetails } from "../../API/movies";
+
 const ProductDetails = () => {
+  const [movie, setMovie] = useState();
+
+  async function setMovieDetails() {
+    const movie = await getMovieDetails(385687);
+    setMovie(movie);
+  }
+
+  useEffect(() => {
+    setMovieDetails();
+  }, []);
+
   return (
     <>
-      <h1>something</h1>
+      <h1>Movie Details</h1>
+      {movie.title}
     </>
   );
 };
